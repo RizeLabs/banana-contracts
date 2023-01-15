cc66bf3ec7506c7dc3194cb91ab53f650a36399c
nlok5923 <nlokhande5923@gmail.com>
2023-01-15 10:33:06 +0530

    updated hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,40 @@
+
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan"
+import "hardhat-gas-reporter"
+
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
+
+
+export default {
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    mumbai: {
+      url: process.env.MUMBAI_URL || '',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+      
+  },
+  goerli: {
+    url: `https://eth-goerli.g.alchemy.com/v2/V5p1PckEwUqIq5s5rA2zvwRKH0V9Hslr`,
+    accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : ["temp"]
+  }
+  },
+  etherscan: {
+    apiKey: "2S8CM6KUUPXGG7JV63UZVVVZTWP6RYJXYE"
+ }
+};
