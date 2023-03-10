8a7b528ca340edd22945545970d42518d2292724
Rishabh Gupta <rishotics@gmail.com>
2023-03-10 18:43:49 +0530

    add new aa contracts

--- contracts/core/BasePaymaster.sol
+++ contracts/core/BasePaymaster.sol
@@ -7,6 +7,7 @@ pragma solidity ^0.8.12;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "../interfaces/IPaymaster.sol";
 import "../interfaces/IEntryPoint.sol";
+import "./Helpers.sol";
 
 /**
  * Helper class for creating a paymaster.
@@ -15,18 +16,23 @@ import "../interfaces/IEntryPoint.sol";
  */
 abstract contract BasePaymaster is IPaymaster, Ownable {
 
-    IEntryPoint public entryPoint;
+    IEntryPoint immutable public entryPoint;
 
     constructor(IEntryPoint _entryPoint) {
-        setEntryPoint(_entryPoint);
-    }
-
-    function setEntryPoint(IEntryPoint _entryPoint) public onlyOwner {
         entryPoint = _entryPoint;
     }
 
-    function validatePaymasterUserOp(UserOperation calldata userOp, bytes32 requestId, uint256 maxCost) external virtual override returns (bytes memory context);
+    /// @inheritdoc IPaymaster
+    function validatePaymasterUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 maxCost)
+    external override returns (bytes memory context, uint256 validationData) {
+         _requireFromEntryPoint();
+        return _validatePaymasterUserOp(userOp, userOpHash, maxCost);
+    }
 
+    function _validatePaymasterUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 maxCost)
+    internal virtual returns (bytes memory context, uint256 validationData);
+
+    /// @inheritdoc IPaymaster
     function postOp(PostOpMode mode, bytes calldata context, uint256 actualGasCost) external override {
         _requireFromEntryPoint();
         _postOp(mode, context, actualGasCost);
@@ -69,10 +75,10 @@ abstract contract BasePaymaster is IPaymaster, Ownable {
     /**
      * add stake for this paymaster.
      * This method can also carry eth value to add to the current stake.
-     * @param extraUnstakeDelaySec - set the stake to the entrypoint's default unstakeDelay plus this value.
+     * @param unstakeDelaySec - the unstake delay for this paymaster. Can only be increased.
      */
-    function addStake(uint32 extraUnstakeDelaySec) external payable onlyOwner {
-        entryPoint.addStake{value : msg.value}(entryPoint.unstakeDelaySec() + extraUnstakeDelaySec);
+    function addStake(uint32 unstakeDelaySec) external payable onlyOwner {
+        entryPoint.addStake{value : msg.value}(unstakeDelaySec);
     }
 
     /**
@@ -101,6 +107,6 @@ abstract contract BasePaymaster is IPaymaster, Ownable {
 
     /// validate the call is made from a valid entrypoint
     function _requireFromEntryPoint() internal virtual {
-        require(msg.sender == address(entryPoint));
+        require(msg.sender == address(entryPoint), "Sender not EntryPoint");
     }
 }
---
+++ contracts/samples/NewTouchIdAccount.sol
@@ -0,0 +1,252 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+/* solhint-disable avoid-low-level-calls */
+/* solhint-disable no-inline-assembly */
+/* solhint-disable reason-string */
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+import "../core/BaseAccount.sol";
+import "./EllipticCurve.sol";
+
+interface IVerifier {
+    function verifyProof(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[2] memory input
+    ) external view returns (bool);
+}
+
+/**
+ * minimal account.
+ *  this is sample minimal account.
+ *  has execute, eth handling methods
+ *  has a single signer that can send requests through the entryPoint.
+ */
+contract NewTouchIdAccount is BaseAccount, UUPSUpgradeable, Initializable {
+    using ECDSA for bytes32;
+
+    //filler member, to push the nonce and owner to the same slot
+    // the "Initializeble" class takes 2 bytes in the first slot
+    bytes28 private _filler;
+
+    //explicit sizes of nonce, to fit a single storage cell with "owner"
+    uint96 private _nonce;
+    address public owner;
+
+    IEntryPoint private immutable _entryPoint;
+    uint[2] qValues;
+    address ellipticCurve;
+
+    event SimpleAccountInitialized(
+        IEntryPoint indexed entryPoint,
+        address indexed owner
+    );
+
+    modifier onlyOwner() {
+        _onlyOwner();
+        _;
+    }
+
+    /// @inheritdoc BaseAccount
+    function nonce() public view virtual override returns (uint256) {
+        return _nonce;
+    }
+
+    /// @inheritdoc BaseAccount
+    function entryPoint() public view virtual override returns (IEntryPoint) {
+        return _entryPoint;
+    }
+
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable {}
+
+    constructor(IEntryPoint anEntryPoint) {
+        _entryPoint = anEntryPoint;
+        _disableInitializers();
+    }
+
+    function _onlyOwner() internal view {
+        //directly from EOA owner, or through the account itself (which gets redirected through execute())
+        require(
+            msg.sender == owner || msg.sender == address(this),
+            "only owner"
+        );
+    }
+
+    // called by entryPoint, only after validateUserOp succeeded.
+    function execFromEntryPoint(
+        address dest,
+        uint256 value,
+        bytes calldata func
+    ) external {
+        // _requireFromEntryPoint();
+        // (uint256[2] memory a, uint256[2][2] memory b, uint256[2] memory c, uint256[2] memory d) = abi.decode(
+        //   func,
+        //   (uint256[2], uint256[2][2], uint256[2], uint256[2])
+        // );
+        // testTransfer(a, b, c, d, dest, value);
+        _call(dest, value, func);
+    }
+
+    function exec(
+        address dest,
+        uint256 value,
+        bytes[] calldata func
+    ) external onlyOwner {
+        (
+            uint256[2] memory a,
+            uint256[2][2] memory b,
+            uint256[2] memory c,
+            uint256[2] memory input
+        ) = abi.decode(
+                func[1],
+                (uint256[2], uint256[2][2], uint256[2], uint256[2])
+            );
+        // require(isProofValid(a, b, c, input), "Invalid proof");
+        _call(dest, value, func[0]);
+    }
+
+    /**
+     * execute a sequence of transaction
+     */
+    function execBatch(
+        address[] calldata dest,
+        bytes[] calldata func
+    ) external onlyOwner {
+        require(dest.length == func.length, "wrong array lengths");
+        for (uint256 i = 0; i < dest.length; i++) {
+            _call(dest[i], 0, func[i]);
+        }
+    }
+
+    /**
+     * execute a transaction (called directly from owner, or by entryPoint)
+     */
+    function execute(
+        address dest,
+        uint256 value,
+        bytes calldata func
+    ) external {
+        _requireFromEntryPointOrOwner();
+        _call(dest, value, func);
+    }
+
+    /**
+     * execute a sequence of transactions
+     */
+    function executeBatch(
+        address[] calldata dest,
+        bytes[] calldata func
+    ) external {
+        _requireFromEntryPointOrOwner();
+        require(dest.length == func.length, "wrong array lengths");
+        for (uint256 i = 0; i < dest.length; i++) {
+            _call(dest[i], 0, func[i]);
+        }
+    }
+
+    /**
+     * @dev The _entryPoint member is immutable, to reduce gas consumption.  To upgrade EntryPoint,
+     * a new implementation of SimpleAccount must be deployed with the new EntryPoint address, then upgrading
+     * the implementation by calling `upgradeTo()`
+     */
+    function initialize(
+        address anOwner,
+        uint[2] memory _qValues,
+        address _ellipticCurve
+    ) public virtual initializer {
+        _initialize(anOwner, _qValues, _ellipticCurve);
+    }
+
+    function _initialize(
+        address anOwner,
+        uint[2] memory _qValues,
+        address _ellipticCurve
+    ) internal virtual {
+        owner = anOwner;
+        qValues = _qValues;
+        ellipticCurve = _ellipticCurve;
+        emit SimpleAccountInitialized(_entryPoint, owner);
+    }
+
+    // Require the function call went through EntryPoint or owner
+    function _requireFromEntryPointOrOwner() internal view {
+        require(
+            msg.sender == address(entryPoint()) || msg.sender == owner,
+            "account: not Owner or EntryPoint"
+        );
+    }
+
+    /// implement template method of BaseAccount
+    function _validateAndUpdateNonce(
+        UserOperation calldata userOp
+    ) internal override {
+        require(_nonce++ == userOp.nonce, "account: invalid nonce");
+    }
+
+    /// implement template method of BaseAccount
+    function _validateSignature(
+        UserOperation calldata userOp,
+        bytes32 userOpHash
+    ) internal virtual override returns (uint256 validationData) {
+        (uint r, uint s, bytes32 message) = abi.decode(
+            userOp.signature,
+            (uint, uint, bytes32)
+        );
+        bool success = EllipticCurve(ellipticCurve).validateSignature(
+            message,
+            [r, s],
+            qValues
+        );
+        // bytes32 hash = userOpHash.toEthSignedMessageHash();
+        if (!success) return SIG_VALIDATION_FAILED;
+        return 0;
+    }
+
+    function _call(address target, uint256 value, bytes memory data) internal {
+        (bool success, bytes memory result) = target.call{value: value}(data);
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+
+    /**
+     * check current account deposit in the entryPoint
+     */
+    function getDeposit() public view returns (uint256) {
+        return entryPoint().balanceOf(address(this));
+    }
+
+    /**
+     * deposit more funds for this account in the entryPoint
+     */
+    function addDeposit() public payable {
+        entryPoint().depositTo{value: msg.value}(address(this));
+    }
+
+    /**
+     * withdraw value from the account's deposit
+     * @param withdrawAddress target to send to
+     * @param amount to withdraw
+     */
+    function withdrawDepositTo(
+        address payable withdrawAddress,
+        uint256 amount
+    ) public onlyOwner {
+        entryPoint().withdrawTo(withdrawAddress, amount);
+    }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal view override {
+        (newImplementation);
+        _onlyOwner();
+    }
+}
